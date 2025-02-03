97f57edef19ef1e21cdf76ec21959bc81f9c09de
shiva <shivangishree020@gmail.com>
2025-02-03 12:36:04 +0530

    some more design added

--- task2/task-2/src/App.js
+++ task2/task-2/src/App.js
@@ -24,7 +24,7 @@ const App = () => {
       <h1 className="main-header">{projectData.title}</h1>
       <div className="image-and-des">
       {projectData.project_image && (
-        <img src={projectData.project_image} alt={projectData.title} style={{ width: "800px", borderRadius: "10px" }} />
+        <img src={projectData.project_image} alt={projectData.title}/>
       )}
       <div className="description">{parse(projectData.description)}</div>
       </div>
@@ -33,39 +33,35 @@ const App = () => {
         {projectData.tasks.map((task) => (
           <div key={task.task_id}>
             <h2 className="task-title">{task.task_title}</h2>
-            <div className="task-des">{parse(task.task_description)}</div>
+            <div className="task-des">{task.task_description}</div>
 
             <div className="asset-container">
             {task.assets.map((asset) => (
               <div className="asset" key={asset.asset_id}>
                 <h3 className="asset-title">{asset.asset_title}</h3>
-                <div className="asset-des">{parse(asset.asset_description)}</div>
+                <div className="asset-des">{asset.asset_description}</div>
                 {asset.asset_content_type === "video" && asset.asset_content ? (
                   <iframe 
                     src={asset.asset_content} 
                     title={asset.asset_title} 
                     width="560" 
                     height="315" 
-                    allowFullScreen
                   ></iframe>
                 ) : asset.asset_content_type === "article" && asset.asset_content ? (
                   <a 
                     href={asset.asset_content} 
-                    target="_blank" 
-                    rel="noopener noreferrer"
                   >
-                    Read more
+                    Click Me to Read More
                   </a>
                 ) : asset.asset_content_type === "threadbuilder" ? (
                   <textarea 
-                    placeholder="Write key threads here..." 
+                    placeholder="Write Key Threads Here" 
                     rows="4"
                     style={{ width: "560", padding: "8px" }}
                   />
                   ) : asset.asset_content_type === "article" &&    !asset.asset_content ? (
                     <textarea 
-                      placeholder="Write your article here..." 
-                      rows="6"
+                      placeholder="Write Article Here" 
                       style={{ width: "100%", padding: "8px" }}
                     />
                   ) : null}
